88c7abd048abfb798d2336a2c8e0b0d28cf8f92b
EC2 Default User <ec2-user@ip-172-31-14-179.ec2.internal>
2021-02-04 11:58:48 +0000

    add_likeOp-start

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+ 
+  devise_for :users
+  resources :users,only: [:show,:index,:edit,:update]
+  root 'homes#top'
+  resources :books
+  get "home/about" => "homes#about"
+  
+end
