554299894a64978f9aa1a37469ac8940b95936d6
EC2 Default User <ec2-user@ip-172-31-14-179.ec2.internal>
2021-02-10 14:50:54 +0000

    Bookers2:add-Ajax_submission

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,13 +6,13 @@ class FavoritesController < ApplicationController
     def create
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
     end
 
     def destroy
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
     end
 
     private
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,14 +1,23 @@
 class SearchesController < ApplicationController
   def search
+
     @range = params[:range]
     @favorite = Favorite.new
     if @range == "User"
       @users = User.looks(params[:searches], params[:words])
     else
       @books = Book.looks(params[:searches], params[:words])
+      # @book = @books.find(params[:id])
 
     end
     @user = current_user
     @favorite = Favorite.new
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
